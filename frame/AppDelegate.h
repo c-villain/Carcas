d41257c554bc44e544e6424b711e28a772c19dd4
Alexander Kraev <c-villain@mail.ru>
2014-09-01 22:00:59 +0400

    Initial Commit

---
+++ frame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  frame
+//
+//  Created by Alexander Kraev on 01.09.14.
+//  Copyright (c) 2014 Alexander Kraev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ frameTests/frameTests.m
@@ -0,0 +1,34 @@
+//
+//  frameTests.m
+//  frameTests
+//
+//  Created by Alexander Kraev on 01.09.14.
+//  Copyright (c) 2014 Alexander Kraev. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface frameTests : XCTestCase
+
+@end
+
+@implementation frameTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
